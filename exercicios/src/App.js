c4a5bee35c846e63469693f1887144ae3ded0920
Lucas Galdino <lucasgaldino7@gmail.com>
2021-12-17 11:07:11 -0300

    17/dec/21 part2

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -1,4 +1,24 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 
-export default () => <Text>Primeiro Componente!</Text>
+import CompPadrao, { Comp1, Comp2 } from './components/Multi'
+import Primeiro from './components/Primeiro'
+
+
+export default () => (
+    <View style={style.App}>
+        <CompPadrao />
+        <Comp1 />
+        <Comp2 />
+        <Primeiro />
+    </View>
+)
+
+const style = StyleSheet.create({
+    App: {
+        flexGrow: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+
+})
