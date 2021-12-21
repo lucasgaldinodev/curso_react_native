80c01d695bea95ed3ed2b82946755747a25bd075
Lucas Galdino <lucasgaldino7@gmail.com>
2021-12-21 10:22:01 -0300

    21/dec/2021

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -1,16 +1,32 @@
 import React from 'react'
 import { View, StyleSheet } from 'react-native'
 
-import CompPadrao, { Comp1, Comp2 } from './components/Multi'
-import Primeiro from './components/Primeiro'
+import Botao from './components/Botao'
+// import Titulo from './components/Titulo'
+// import Aleatorio from './components/Aleatorio'
+// import MinMax from './components/MinMax'
+// import CompPadrao, { Comp1, Comp2 } from './components/Multi'
+// import Primeiro from './components/Primeiro'
 
 
 export default () => (
     <View style={style.App}>
+        <Botao />
+        {/*
+        <Titulo principal="Cadastro Produto"
+            secundario="Tela de Cadastro do Produto" />
+        <Aleatorio min={1} max={60} />
+        <Aleatorio min={1} max={60} />
+        <Aleatorio min={1} max={60} />
+        <Aleatorio min={1} max={60} />
+        <Aleatorio min={1} max={60} />
+        <Aleatorio min={1} max={60} />
+        <MinMax min={3} max={20} />
+        <MinMax min={1} max={94} />
         <CompPadrao />
         <Comp1 />
         <Comp2 />
-        <Primeiro />
+        <Primeiro /> */}
     </View>
 )
 
@@ -18,7 +34,8 @@ const style = StyleSheet.create({
     App: {
         flexGrow: 1,
         justifyContent: "center",
-        alignItems: "center"
+        alignItems: "center",
+        padding: 20
     },
 
 })
